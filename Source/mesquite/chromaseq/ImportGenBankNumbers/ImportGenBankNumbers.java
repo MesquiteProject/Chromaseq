1bf295afcfe9aa7b434694cca9492c3cf628fc0f
wmaddisn <wmaddisn@mail.ubc.ca>
2025-06-06 15:04:23 -0700

    Revised GenBank import system

---
+++ Source/mesquite/chromaseq/ImportGenBankNumbers/ImportGenBankNumbers.java
@@ -0,0 +1,307 @@
+package mesquite.chromaseq.ImportGenBankNumbers;
+
+import java.awt.Button;
+import java.awt.Choice;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.StringTokenizer;
+
+import javax.swing.JEditorPane;
+
+import mesquite.categ.lib.MolecularData;
+import mesquite.chromaseq.lib.GenBankNumbersFileReader;
+import mesquite.externalCommunication.lib.AppInformationFile;
+import mesquite.lib.characters.*;
+import mesquite.lib.*;
+import mesquite.lib.duties.TaxonUtility;
+import mesquite.lib.duties.TextDisplayer;
+import mesquite.lib.misc.VoucherInfoFromOTUIDDB;
+import mesquite.lib.taxa.Taxa;
+import mesquite.lib.ui.AlertDialog;
+import mesquite.lib.ui.ColorDistribution;
+import mesquite.lib.ui.ColorTheme;
+import mesquite.lib.ui.ExtensibleDialog;
+import mesquite.lib.ui.MesquiteWindow;
+import mesquite.lib.ui.RadioButtons;
+
+public class ImportGenBankNumbers extends TaxonUtility implements ItemListener, ActionListener {
+
+	/* ................................................................................................................. */
+	public boolean startJob(String arguments, Object condition,boolean hiredByName) {
+		hireAllEmployees(GenBankNumbersFileReader.class);
+		return true;
+	}
+
+	/* ................................................................................................................. */
+	public String getName() {
+		return "Import Genbank Numbers from File";
+	}
+	/* ................................................................................................................. */
+	public String getNameForMenuItem() {
+		return "Import Genbank Numbers from File...";
+	}
+
+	/* ................................................................................................................. */
+	public String getExplanation() {
+		return "Reads text files of voucher codes or taxon names by genes and imports matches into '\"GenbankStatus\" note.";
+	}
+
+	/* ................................................................................................................. */
+	/** returns whether this module is requesting to appear as a primary choice */
+	public boolean requestPrimaryChoice() {
+		return false;
+	}
+
+	/* ................................................................................................................. */
+	public boolean isPrerelease() {
+		return true;
+	}
+
+	/* ................................................................................................................. */
+	public boolean isSubstantive() {
+		return true;
+	}
+	/* ................................................................................................................. */
+	/** if returns true, then requests to remain on even after operateOnTaxa is called.  Default is false*/
+	public boolean pleaseLeaveMeOn(){
+		return false;
+	}
+	public void itemStateChanged(ItemEvent e) {
+  		if (e.getItemSelectable() == formats){
+  			int which = formats.getSelectedIndex();
+  			explanationOfReader.setText("Explanation of chosen file format:\n" + getChoice(which).getExplanation());
+  			
+  		}
+	}
+	public void actionPerformed(ActionEvent e) {
+  			int which = formats.getSelectedIndex();
+  			String example = ((GenBankNumbersFileReader)getChoice(which)).exampleFile();
+			AlertDialog.noticeHTML(dialog, "Example of file format", example, 400, 300, null);
+
+	}
+
+	MesquiteModule getChoice(int i){
+		if (choices == null)
+			return null;
+		if (i>=0 && i<choices.size())
+			return (MesquiteModule)choices.elementAt(i);
+		return null;
+	}
+	
+	ExtensibleDialog dialog = null;
+	Choice formats;
+	TextArea explanationOfReader = null;
+	JEditorPane examplePane = null;
+	Button exampleButton = null;
+	GenBankNumbersFileReader readerChosen = null;
+	int conflictBehaviour = 0;
+	
+	ListableVector choices = null;
+	public boolean queryOptions(MesquiteModule displayer) {
+		if (!okToInteractWithUser(CAN_PROCEED_ANYWAY, "Querying Options")) 
+			return true;
+		MesquiteInteger buttonPressed = new MesquiteInteger(1);
+		MesquiteWindow w = containerOfModule();
+		if (displayer != null)
+			w = displayer.containerOfModule();
+		dialog = new ExtensibleDialog(w,  "Importing GenBank Numbers",buttonPressed);  //MesquiteTrunk.mesquiteTrunk.containerOfModule()
+		ListableVector emps = getEmployeeVector();
+		choices = new ListableVector();
+		for (int i = 0; i<emps.size(); i++)
+			if (emps.elementAt(i) instanceof GenBankNumbersFileReader)
+				choices.addElement(emps.elementAt(i), false);
+		dialog.addLabel("Format of table in file:");
+		formats = dialog.addPopUpMenu ("Built-in versions:", choices, 0);
+		formats.addItemListener(this);
+		explanationOfReader =dialog.addLargeTextLabel("Explanation of chosen file format:\n\n" + getChoice(0).getExplanation());
+		exampleButton = dialog.addButton("Example");
+		Container parentOfFormats = formats.getParent();
+		Container parentOfButton = exampleButton.getParent();
+		readerChosen = null;
+		parentOfButton.remove(exampleButton);
+		parentOfFormats.add(exampleButton);
+		exampleButton.addActionListener(this);
+	//	examplePane = dialog.addHTMLPanel("<tt>Hello   hello    hello</tt>", 150, 150, null);
+		dialog.addStackedLabels("If you choose to import, and there is already an existing Genbank number", " recorded in the data file, and also an incoming one from the import file:", 0);
+		RadioButtons rb = dialog.addRadioButtons(new String[]{"Ignore Incoming", "Add Incoming to Existing", "Replace Existing by Incoming"},0);
+
+		dialog.addHorizontalLine(1);
+		dialog.addLargeOrSmallTextLabel("You can choose to Import the GenBank numbers according to the options above, or you can choose to see what is available (but not import or change your current data file) by Surveying the file.");
+	
+		dialog.completeAndShowDialog("Survey", "Import", "Cancel", "Cancel");
+		surveyOnly = buttonPressed.getValue()==0;
+		if (buttonPressed.getValue()<2)  { //not cancel
+		  			int which = formats.getSelectedIndex();
+		  			readerChosen =(GenBankNumbersFileReader)getChoice(which);
+		  			conflictBehaviour = rb.getValue();
+		}
+		dialog.dispose();
+		return (buttonPressed.getValue()<2);
+	}
+
+	
+	NameReference genBankColor = NameReference.getNameReference("genbankcolor");
+	boolean surveyOnly = true;
+	/** Called to operate on the taxa in the block.  Returns true if taxa altered*/
+	public boolean operateOnTaxa(Taxa taxa){
+		MesquiteFile file = 	MesquiteFile.open(true, (String)null, "Choose text file with GenBank number information", null);
+
+		if (file == null)
+			return false;
+		String path = file.getPath();
+		String preview = MesquiteFile.getFileContentsAsString(path, 4000); 
+		if (StringUtil.blank(preview))
+			return false;
+		TextDisplayer displayer = (TextDisplayer)hireEmployee(TextDisplayer.class, null);
+		if (displayer!=null){
+			displayer.showText(preview, "File contents", true); 
+		}
+		
+		//Now present dialog with choices
+		if (!queryOptions(displayer)) {
+			fireEmployee(displayer);
+			return false;
+		}
+		fireEmployee(displayer);
+		
+		log("If GenBank information already exists in data file, any incoming GenBank information will ");
+		if (conflictBehaviour == 0)
+			logln("be ignored");
+		else if (conflictBehaviour == 1)
+			logln("be added to the information already existing");
+		else if (conflictBehaviour == 2)
+			logln("replace the information already existing");
+	
+		String[][] table = readerChosen.readTable(path);
+		
+		Debugg.println(StringArray.toString(table));
+		if (table == null || table.length == 0)
+			return false;
+	
+		int count = 0;
+		boolean anySelected = taxa.anySelected();
+		if (anySelected)
+			logln("Only those taxa that are selected will be considered.");
+		String thoseNotFound = "";
+		boolean colored = false;
+		for (int row = 0; row< table.length; row++){
+			int it = findTaxon(taxa, table[row][GenBankNumbersFileReader.ID]);
+			if (it>=0 && (!anySelected || taxa.getSelected(it))){
+					CharacterData matrix =  getProject().getCharacterMatrixReverseOrder((MesquiteFile)null, taxa, null, table[row][GenBankNumbersFileReader.GENE]);
+					if (matrix != null &&  !StringUtil.blank(table[row][GenBankNumbersFileReader.GENBANK])){
+						Associable associable = matrix.getTaxaInfo(false);  //the metadata is associated with this, not with the matrix directly
+						String current = (String)associable.getAssociatedString(MolecularData.genBankNumberRef,  it);
+						associable.setAssociatedObject(genBankColor,  it, null);  //reset to no color before starting
+						if (surveyOnly || conflictBehaviour != 0 || StringUtil.blank(current)){
+							String incoming = StringUtil.stripBoundingWhitespace(table[row][GenBankNumbersFileReader.GENBANK]);
+							String incomingCompact = StringUtil.stripWhitespace(incoming);
+							String currentCompact = StringUtil.stripWhitespace(current);
+							Color color = null;
+							if (!StringUtil.blank(incoming) && incoming.length() > 1){
+								if (StringUtil.blank(current)){
+									current = incoming;
+									logln("--- Genbank number \"" + incoming + "\" to be acquired for taxon " + taxa.getTaxonName(it) + " (" + table[row][GenBankNumbersFileReader.ID] + ")");
+									if (surveyOnly)
+										color = Color.yellow;
+									else
+										color = ColorDistribution.veryLightYellow;
+								}
+								else if (current.length() == 1 && incoming.length()>1){ //current is just one character, a placeholder; ignore it and replace by incoming
+									current = incoming;
+									logln("--- Genbank number \"" + incoming + "\" to be acquired for taxon " + taxa.getTaxonName(it) + " (" + table[row][GenBankNumbersFileReader.ID] + ")");
+									if (surveyOnly)
+										color = Color.yellow;
+									else
+										color = ColorDistribution.veryLightYellow;
+								}
+								else if (incoming.indexOf(current)>= 0 && incoming.length() > current.length()){  //incoming is superstring of current; replace by current
+									current = incoming;
+									logln("--- Genbank number \"" + incoming + "\" to replace previous \"" + current + "\" for taxon " + taxa.getTaxonName(it) + " (" + table[row][GenBankNumbersFileReader.ID] + ")");
+									if (surveyOnly)
+										color = Color.magenta;
+									else
+										color = ColorDistribution.veryLightYellow;
+								}
+								else if (current.equals(incoming)){  //same; do nothing
+									logln("    Genbank number \"" + incoming + "\" same as previous \"" + current + "\" for taxon " + taxa.getTaxonName(it) + " (" + table[row][GenBankNumbersFileReader.ID] + ")");
+									if (surveyOnly)
+										color = ColorDistribution.veryLightGreen;
+								}
+								else if (current.indexOf(incoming)>= 0){  //already included
+									logln("    Genbank number \"" + incoming + "\" already included in previous \"" + current + "\" for taxon " + taxa.getTaxonName(it) + " (" + table[row][GenBankNumbersFileReader.ID] + ")");
+									if (surveyOnly)
+										color = ColorDistribution.veryLightGreen;
+								}
+								else if (!StringUtil.blank(incomingCompact) && incomingCompact.indexOf(currentCompact)<0){  
+									if (conflictBehaviour == 1){
+										logln("#-- Genbank number \"" + incoming + "\" to append to previous \"" + current + "\" for taxon " + taxa.getTaxonName(it) + " (" + table[row][GenBankNumbersFileReader.ID] + ")");
+										current = current + ";" + incoming;
+									}
+									else {
+										logln("#-- Genbank number \"" + incoming + "\" to replace previous \"" + current + "\" for taxon " + taxa.getTaxonName(it) + " (" + table[row][GenBankNumbersFileReader.ID] + ")");
+										current =  incoming;
+										}
+									if (surveyOnly)
+										color = Color.magenta;
+									else
+										color = ColorDistribution.veryLightYellow;
+							}
+								if (!surveyOnly)
+									associable.setAssociatedString(MolecularData.genBankNumberRef,  it, StringUtil.stripBoundingWhitespace(current));
+							}
+							if (color != null) {
+								colored = true;
+								associable.setAssociatedObject(genBankColor,  it, color);  //color is not saved
+							}
+							count++;
+						}
+					
+				}
+			}
+			else if (!StringUtil.blank(table[row][GenBankNumbersFileReader.ID]))
+				thoseNotFound += " " + table[row][GenBankNumbersFileReader.ID];
+
+		}
+		if (colored) {
+			ListableVector datas = getProject().getCharacterMatrices();
+			datas.notifyListeners(this, new Notification(MesquiteListener.ANNOTATION_CHANGED));
+		}
+		logln("Genbank status imported.  Number of matches found: " + count);
+		if (!StringUtil.blank(thoseNotFound))
+			logln("Taxon ID's in table but not found in taxa block: " + thoseNotFound);
+		return true;
+	}
+	private int findTaxon(Taxa taxa, String id){
+		if (StringUtil.blank(id))
+			return -1;
+		for (int it =0; it<taxa.getNumTaxa(); it++){
+			String code = taxa.getAssociatedString(VoucherInfoFromOTUIDDB.voucherCodeRef, it);
+			if (!StringUtil.blank(code)){
+				if (code.equalsIgnoreCase(id))
+					return it;
+				StringTokenizer tokenizer = new StringTokenizer(code, "/");
+				while (tokenizer.hasMoreElements()){
+					String token = tokenizer.nextToken();
+					if (!StringUtil.blank(token) && token.equalsIgnoreCase(id))
+						return it;
+				}
+				if (id.indexOf("/")>=0){
+					if (StringUtil.indexOfIgnoreCase(code, id)>=0)
+						return it;
+				}
+			}
+			String name = taxa.getTaxonName( it);
+			if (!StringUtil.blank(name) && name.equalsIgnoreCase(id))
+				return it;
+
+		}
+		return -1;
+	}
+
+
+}
---
+++ Source/mesquite/chromaseq/lib/GenBankNumbersFileReader.java
@@ -0,0 +1,46 @@
+/* Mesquite source code.  Copyright 1997 and onward, W. Maddison and D. Maddison. 
+
+
+Disclaimer:  The Mesquite source code is lengthy and we are few.  There are no doubt inefficiencies and goofs in this code. 
+The commenting leaves much to be desired. Please approach this source code with the spirit of helping out.
+Perhaps with your help we can be more than a few, and make Mesquite better.
+
+Mesquite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+Mesquite's web site is http://mesquiteproject.org
+
+This source code and its compiled class files are free and modifiable under the terms of 
+GNU Lesser General Public License.  (http://www.gnu.org/copyleft/lesser.html)
+*/
+package mesquite.chromaseq.lib;
+
+import java.awt.*;
+import java.util.*;
+import mesquite.lib.*;
+import mesquite.lib.duties.*;
+import mesquite.lib.taxa.Taxa;
+
+
+
+/* ======================================================================== */
+public abstract class GenBankNumbersFileReader extends MesquiteModule  {
+
+   	 public Class getDutyClass() {
+   	 	return GenBankNumbersFileReader.class;
+   	 }
+ 	public String getDutyName() {
+ 		return "GenBank Numbers File Reader";
+   	 }
+
+ 	public static int ID = 0;
+ 	public static 	int GENE = 1;
+ 	public static 	int GENBANK = 2;
+   	/** Called to read the text file, and return an n x 3 String array, with
+   	 * column 0 -- voucher ID or taxon name. (System will look to match voucherID, but then use taxon name otherwise)
+   	 * column 1 -- gene name
+   	 * column 2 -- GenBank number(s)*/
+   	public abstract String[][] readTable(String pathToFile);
+
+   	/* Return an example file for display as monospace, so use spaces to line up columns etc. */
+   	public abstract String exampleFile();
+}
+
